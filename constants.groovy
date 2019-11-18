de6e2c9113f86ac04dc75419c40d083350ec03d0
shweta9651 <49896512+shweta9651@users.noreply.github.com>
2019-11-18 14:52:45 +0530

    Update constants.groovy

--- constants.groovy
+++ constants.groovy
@@ -1,6 +1,7 @@
 import groovy.transform.Field
 
 @Field
-def giturl = 'https://github.com/shweta9651/java-junit-sample.git'
+//def giturl = 'https://github.com/shweta9651/java-junit-sample.git'
+def giturl = 'https://github.com/shweta965'
 
 return this;
