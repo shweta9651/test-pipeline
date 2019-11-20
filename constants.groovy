17ecb4ef8cd3bf620ccaaa9df68536f8272f48e6
shweta9651 <49896512+shweta9651@users.noreply.github.com>
2019-11-20 16:58:42 +0530

    Update constants.groovy

--- constants.groovy
+++ constants.groovy
@@ -1,6 +1,6 @@
 import groovy.transform.Field
 
 @Field
-def giturl = 'https://github.com/shweta9651/java-junit-sample.git'
+def giturl = 'https://github.com/shweta9651/ansible-poc.git'
 
 return this;
